07728bc9acb9e59fe460ac4532938c2c6dfbdc1b
Павел <pashanoy@gmail.com>
2022-06-04 16:39:01 +0300

    improve benchmarking with many runs and taking avg

--- benchmark.py
+++ benchmark.py
@@ -62,15 +62,27 @@ def benchmark(formula_length: int, formula_coeff_modulo_limit: int):
     result.append((time.perf_counter_ns() - start) // runs)
     result.append(solving_automata.state_counter)
 
-    print(f"formula_length = {formula_length}, modulo_max = {formula_coeff_modulo_limit}")
-    print(f"naive solve time: {result[0]}ns, automata build time: {result[1]}ns, automata solve time: {result[2]}ns, automata state amount: {result[3]}")
+    return result
 
-benchmark(3, 10)
-benchmark(3, 20)
-benchmark(3, 40)
-benchmark(5, 10)
-benchmark(5, 20)
-benchmark(8, 10)
-benchmark(10, 10)
-benchmark(12, 7)
-benchmark(15, 5)
+
+def benchmark_improved(formula_length: int, formula_coeff_modulo_limit: int):
+    runs_amount = 100
+
+    results = [benchmark(formula_length, formula_coeff_modulo_limit) for _ in range(runs_amount)]
+
+    restructured_results = [[results[i][j] for i in range(len(results))] for j in range(len(results[0]))]
+
+    final_results = [sum(a) // runs_amount for a in restructured_results]
+
+    print(f"formula_length = {formula_length}, modulo_max = {formula_coeff_modulo_limit}")
+    print(f"naive solve time: {final_results[0]}ns, automata build time: {final_results[1]}ns, automata solve time: {final_results[2]}ns, automata state amount: {final_results[3]}")
+
+benchmark_improved(3, 10)
+benchmark_improved(3, 20)
+benchmark_improved(3, 40)
+benchmark_improved(5, 10)
+benchmark_improved(5, 20)
+benchmark_improved(8, 10)
+benchmark_improved(10, 10)
+benchmark_improved(12, 7)
+#benchmark_improved(15, 5)
